c5418a5a6395b6dc47a80869dcec13865460e0cd
andreas4all <andreas4all@users.noreply.github.com>
2019-03-28 15:01:43 +0100

    Symlink with force

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,13 @@ if [ ! -f /etc/ssh/ssh_host_rsa_key ] && [ ! -f /etc/ssh/ssh_host_dsa_key ] && [
     mv /etc/ssh/ssh_host_ed25519_key.pub /root/sshd
 fi
 
-ln -s /root/sshd/ssh_host_rsa_key /etc/ssh
-ln -s /root/sshd/ssh_host_rsa_key.pub /etc/ssh
-ln -s /root/sshd/ssh_host_dsa_key /etc/ssh
-ln -s /root/sshd/ssh_host_dsa_key.pub /etc/ssh
-ln -s /root/sshd/ssh_host_ecdsa_key /etc/ssh
-ln -s /root/sshd/ssh_host_ecdsa_key.pub /etc/ssh
-ln -s /root/sshd/ssh_host_ed25519_key /etc/ssh
-ln -s /root/sshd/ssh_host_ed25519_key.pub /etc/ssh
+ln -s -f /root/sshd/ssh_host_rsa_key /etc/ssh
+ln -s -f /root/sshd/ssh_host_rsa_key.pub /etc/ssh
+ln -s -f /root/sshd/ssh_host_dsa_key /etc/ssh
+ln -s -f /root/sshd/ssh_host_dsa_key.pub /etc/ssh
+ln -s -f /root/sshd/ssh_host_ecdsa_key /etc/ssh
+ln -s -f /root/sshd/ssh_host_ecdsa_key.pub /etc/ssh
+ln -s -f /root/sshd/ssh_host_ed25519_key /etc/ssh
+ln -s -f /root/sshd/ssh_host_ed25519_key.pub /etc/ssh
 
 passwd postgres -u && /usr/sbin/sshd -e && /usr/local/bin/docker-entrypoint.sh postgres
